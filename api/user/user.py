4b4df35f47555565fd8c2d948b0ae497dc2e43a5
ImTaeuk <100995244+ImTaeuk@users.noreply.github.com>
2023-02-07 01:06:52 +0900

    Update recommend status code modding

--- api/user/user.py
+++ api/user/user.py
@@ -22,13 +22,16 @@ class UserManagement(Resource):
 
         if (row is None):
             db.close()
-            return {"message" : "아이디나 비밀번호 불일치"}
+            return make_response(jsonify({"message" : "해당 유저가 존재하지 않음"}), 400)
+
         else:
             if (outpw != pw):
                 db.close()
-                return {"message" : "아이디나 비밀번호 불일치"}
+                return make_response(jsonify({"message" : "아이디나 비밀번호 불일치"}), 400)
+
             else:
-                return {"nickname" : outnickname}
+                return make_response(jsonify({"nickname" : outnickname}), 200)
+
 
 
     def post(self):
@@ -50,12 +53,12 @@ class UserManagement(Resource):
             db.commit()
             db.close()
 
-            return {"is_success" : True, "message" : "유저생성성공"}
+            return make_response(jsonify({"is_success" : True, "message" : "유저생성성공"}), 200)
 
         else:
             db.close()
+            return make_response(jsonify({"is_success" : False, "message" : "이미 있는 유저"}), 400)
 
-            return {"is_success" : False, "message" : "유저생성실패"}
 
 
     def put(self):
@@ -69,26 +72,32 @@ class UserManagement(Resource):
         pw = data['pw']
         nickname = data['nickname']
 
-        sql = "SELECT id, pw FROM user WHERE id = '" + id + "'"
+        sql = "SELECT id, pw, nickname FROM user WHERE id = '" + id + "'"
         
         row = db.execute_one(sql)
-        print(row)
+
+        outnickname = row['nickname']
         outpw = row['pw']
 
         if (row is None):
             db.close()
-            return {"is_success" : False, "message" : "유저닉네임변경실패"}
+            return make_response(jsonify({"is_success" : False, "message" : "아이디나 비밀번호 불일치"}), 400)
         else:
             if (outpw != pw):
                 db.close()
-                return {"is_success" : False, "message" : "유저닉네임변경실패"}
+                return make_response(jsonify({"is_success" : False, "message" : "아이디나 비밀번호 불일치"}), 400)
+            
+            elif (outnickname == nickname):
+                 return make_response(jsonify({"is_success" : False, "message" : "현재 닉네임과 같음"}), 400)
+            
             else:
                 sql = "UPDATE taeukDB.user SET nickname = '" + nickname + "' WHERE id = '" + id + "'"
                 db.execute(sql)
                 db.commit()
                 db.close()
 
-                return {"is_success" : True, "message" : "유저닉네임변경성공"}
+                return make_response(jsonify({"is_success" : True, "message" : "유저 닉네임 변경 성공"}), 200)
+
     
     def delete(self):
         # DELETE method 구현 부분
@@ -107,15 +116,15 @@ class UserManagement(Resource):
 
         if (row is None):
             db.close()
-            return {"is_success" : False, "message" : "유저삭제실패"}
+            return make_response(jsonify({"is_success" : False, "message" : "아이디나 비밀번호 불일치"}), 400)
+
         else:
             if (outpw != pw):
                 db.close()
-                return {"is_success" : False, "message" : "유저삭제실패"}
+                return make_response(jsonify({"is_success" : False, "message" : "아이디나 비밀번호 불일치"}), 400)
             else:
                 sql = "DELETE FROM taeukDB.user WHERE id = '" + id + "'"
                 db.execute(sql)
                 db.commit()
                 db.close()
-
-                return {"is_success" : True, "message" : "유저삭제성공"}
+                return make_response(jsonify({"is_success" : True, "message" : "유저 삭제 성공"}), 200)
